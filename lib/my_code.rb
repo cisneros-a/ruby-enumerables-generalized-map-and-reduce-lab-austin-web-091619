0ed7f2677a390c8f0b4dc02e30d5145e52ba9cea
Adrian Cisneros <cisneros_adrian51@yahoo.com>
2019-09-14 14:39:36 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -15,3 +15,13 @@ end
 map([12, 32, -4, 27, -64]) { |n| n * 1 }
 
 map([12, 32, -4, 27, -64]){ |n| n * 2}
+
+def reduce_to_total(source_array, starting_point = 0)
+  i = 0
+  new = starting_point
+  while i < source_array.length do
+    new += source_array[i]
+    i+= 1
+  end
+  return new
+end
