02c26163e11a1e23d89d87ab46592d051cd3dd93
Adrian Cisneros <cisneros_adrian51@yahoo.com>
2019-09-14 14:52:48 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -15,19 +15,3 @@ end
 map([12, 32, -4, 27, -64]) { |n| n * 1 }
 
 map([12, 32, -4, 27, -64]){ |n| n * 2}
-
-
-def reduce(array, starting_point)
-  if starting_point
-    sum = starting_point
-    i = 0
-  else
-    sum = array[0]
-    i = 1
-  end
-  while i < array.length do
-    sum = yeild(sum, array[i])
-    i +=1
-  end
-  return sum
-end
