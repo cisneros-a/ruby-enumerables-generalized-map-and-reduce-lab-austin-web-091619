cd75c084a1d0578cd331f5768e480787e39170dc
Adrian Cisneros <cisneros_adrian51@yahoo.com>
2019-09-14 14:11:07 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,18 @@
+def map(array)
+  new = []
+  i = 0
+  while i < array.length
+    new.push(yield(array[i]))
+    i += 1
+  end
+  p new
+end
+
+map([12, 32, -4, 27, -64]) do
+  |n| n * -1
+end
+
+map([12, 32, -4, 27, -64]) { |n| n * 1 }
+
+map([12, 32, -4, 27, -64]){ |n| n * 2}
 
