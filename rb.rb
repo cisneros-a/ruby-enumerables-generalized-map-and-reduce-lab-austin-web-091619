75d5bd6eb233e31217d5bcda3edd0dccbe73d6bc
Adrian Cisneros <cisneros_adrian51@yahoo.com>
2019-09-14 13:26:34 +0000

    Automatically backed up by Learn

--- rb.rb
+++ rb.rb
@@ -23,3 +23,38 @@ map_to_negativize(array) {|n|
   end
   return new
 }
+
+source_array = [31, 43, -4, 6]
+def map_to_negativize(source_array)
+  new = []
+  i = 0
+  while i <source_array.length do
+    new.push(source_array[i] * -1)
+    i += 1
+  end
+  return new
+end
+
+def map_to_no_change(source_array)
+  return source_array
+end
+
+def map_to_double(source_array)
+  new = []
+  i = 0
+  while i <source_array.length do
+    new.push(source_array[i] * 2)
+    i += 1
+  end
+  return new
+end
+
+def map_to_square(source_array)
+  new = []
+  i = 0
+  while i <source_array.length do
+    new.push(source_array[i] * source_array[i])
+    i += 1
+  end
+  return new
+end
